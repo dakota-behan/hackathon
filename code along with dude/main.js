438040baf63e839a0b953cd16f77dea183f0298e
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-07-11 12:42:38 -0700

    trying to make three.js sandbox

---
+++ code along with dude/main.js
@@ -0,0 +1,18 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
+
+// npm install --save three
+// npm install --save-dev vite
+// npx vite
+// npx serve .
+
+console.log('You are connected')
+console.log('\n---------------\n')
+// const test1 = document.getElementById('first')
+// const test2 = document.getElementById('test')
+// console.log(test1.innerText)
+// console.log(test2.innerText)
